d10fcdb4e9af6b4b30be6ebc7a1e258fbac8f61a
Johanna Raisa <johanna.raisa@gmail.com>
2018-10-26 13:07:39 +0300

    Adds formatxml

--- lib/KohaSuomiServices/Model/Convert.pm
+++ lib/KohaSuomiServices/Model/Convert.pm
@@ -39,33 +39,64 @@ sub formatjson {
     }
 }
 
+sub formatxml {
+    my ($self, $marcjson) = @_;
+
+    try {
+        my $format = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+        $format .= "<record>\n";
+        $format .= "\t<leader>".$marcjson->{leader}."</leader>\n" if ($marcjson->{leader});
+        foreach my $field (@{$marcjson->{fields}}) {
+            if (defined $field->{value}) {
+                $format .= "\t<controlfield tag=\"".$field->{tag}."\">".$field->{value}."</controlfield>\n";
+            } else {
+                $format .= "\t<datafield tag=\"".$field->{tag}."\" ind1=\"".$field->{ind1}."\" ind2=\"".$field->{ind2}."\">\n";
+                foreach my $subfield (@{$field->{subfields}}) {
+                    $format .= "\t\t<subfield code=\"".$subfield->{code}."\">".$subfield->{value}."</subfield>\n";
+                }
+                $format .= "\t</datafield>\n";
+            }
+        }
+        $format .= "</record>";
+        return $format;
+
+    } catch {
+        my $e = $_;
+        return $e->{message};
+    }
+}
+
 sub formatfields {
     my ($self, $controlfields, $datafields) = @_;
 
     my @fields;
-
+    my %filters = ("999" => "1", "942" => 1, "852" => 1);
     foreach my $controlfield (@{$controlfields}) {
-        my $formated;
-        $formated->{tag} = $controlfield->{"tag"};
-        $formated->{value} = $controlfield->{"content"};
-        push @fields, $formated;
+        if (!$filters{$controlfield->{"tag"}}) {
+            my $formated;
+            $formated->{tag} = $controlfield->{"tag"};
+            $formated->{value} = $controlfield->{"content"};
+            push @fields, $formated;
+        }
     }
 
     foreach my $datafield (@{$datafields}) {
         my $formated;
         my @subfields;
-        $formated->{tag} = $datafield->{"tag"};
-        $formated->{ind1} = $datafield->{"ind1"};
-        $formated->{ind2} = $datafield->{"ind2"};
-        if (ref($datafield->{"subfield"}) eq "HASH"){
-            push @subfields, {code => $datafield->{"subfield"}->{"code"}, value => $datafield->{"subfield"}->{"content"}}
-        } else {
-            foreach my $subfield (@{$datafield->{"subfield"}}) {
-                push @subfields, {code => $subfield->{"code"}, value => $subfield->{"content"}}
+        if (!$filters{$datafield->{"tag"}}) {
+            $formated->{tag} = $datafield->{"tag"};
+            $formated->{ind1} = $datafield->{"ind1"};
+            $formated->{ind2} = $datafield->{"ind2"};
+            if (ref($datafield->{"subfield"}) eq "HASH"){
+                push @subfields, {code => $datafield->{"subfield"}->{"code"}, value => $datafield->{"subfield"}->{"content"}}
+            } else {
+                foreach my $subfield (@{$datafield->{"subfield"}}) {
+                    push @subfields, {code => $subfield->{"code"}, value => $subfield->{"content"}}
+                }
             }
+            $formated->{subfields} = \@subfields;
+            push @fields, $formated;
         }
-        $formated->{subfields} = \@subfields;
-        push @fields, $formated;
     }
 
     return \@fields;
