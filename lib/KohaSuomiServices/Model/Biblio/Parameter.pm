e353e63585bcc18755a58b1f24bc9a6cb4e6d541
Johanna Raisa <johanna.raisa@gmail.com>
2018-10-31 11:12:00 +0200

    Adds parameter exception

--- lib/KohaSuomiServices/Model/Biblio/Parameter.pm
+++ lib/KohaSuomiServices/Model/Biblio/Parameter.pm
@@ -8,6 +8,8 @@ use Mojo::UserAgent;
 use KohaSuomiServices::Model::Convert;
 use Mojo::JSON qw(decode_json encode_json);
 
+use KohaSuomiServices::Model::Exception::NotFound;
+
 has schema => sub {KohaSuomiServices::Database::Client->new};
 has config => sub {KohaSuomiServices::Model::Config->new->service("biblio")->load};
 
@@ -17,6 +19,7 @@ sub find {
     my $client = $self->schema->client($self->config);
     my @p = $client->resultset("Parameter")->search($params);
     my $interfaceParams = $self->schema->get_columns(@p);
+    KohaSuomiServices::Model::Exception::NotFound->throw(error => "No interface parameters") unless $interfaceParams;
     return $interfaceParams;
 }
 
