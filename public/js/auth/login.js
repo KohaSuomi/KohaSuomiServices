89a3ec8c9748328b7e8b8036b6edf1147af7e9c8
Johanna Raisa <johanna.raisa@gmail.com>
2018-11-19 11:07:36 +0200

    Moves login to folder

---
+++ public/js/auth/login.js
@@ -0,0 +1,31 @@
+new Vue({
+    el: '#loginApp',
+    data: {
+        errors: [],
+        username: "",
+        password: "",
+    },
+    methods: {
+        login() {
+        var form = new FormData();
+        form.set("userid", this.username);
+        form.set("password", this.password);
+        axios.post(loginpath, form
+        ).then(response => {
+            this.setSession(response.data);
+          }).catch(error => {
+            this.errors = [];
+            this.errors.push(error.response.data.error);
+          });
+        },
+        setSession(data) {
+        axios.post(baseendpoint+'auth', data, {headers: { Authorization: apikey }, withCredentials: true}
+        ).then(response => {
+            window.location.href = window.location.origin;
+          }).catch(error => {
+            this.errors = [];
+            this.errors.push(error.response.data.error);
+          });
+        }
+    }
+});
