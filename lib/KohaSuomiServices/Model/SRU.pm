0a8b8f52380ca5627e98ad95f3e84623f1ea4c43
Johanna Raisa <johanna.raisa@gmail.com>
2018-11-20 15:47:16 +0200

    Adds check for sru record

--- lib/KohaSuomiServices/Model/SRU.pm
+++ lib/KohaSuomiServices/Model/SRU.pm
@@ -18,7 +18,6 @@ sub search {
     my $path = $params->{url}."?operation=".$params->{operation}."&query=".$params->{query};
     $path .= defined $params->{version} ? "&version=".$params->{version} : "&version=1.1";
     $path .= defined $params->{maxrecords} ? "&maximumRecords=".$params->{maxrecords} : "&maximumRecords=1";
-    
     my $tx = $self->ua->build_tx(GET => $path);
     $tx = $self->ua->start($tx);
     my $records = $self->getRecords($tx->res->body);
@@ -33,17 +32,19 @@ sub getRecords {
         my $fields;
         my @records;
         my $records = first { m/records/ } keys %{$xml};
-        my $record = first { m/record/ } keys %{$xml->{$records}};
-        my $recordData = first { m/recordData/ } keys %{$xml->{$records}->{$record}};
-        my $marcrecord = first { m/record/ } keys %{$xml->{$records}->{$record}->{$recordData}};
-        
-        if (ref($xml->{$records}->{$record}) eq "HASH") {
-            my $data = $xml->{$records}->{$record}->{$recordData}->{$marcrecord};
-            push @records, $self->convert->formatjson($data);
-        } else {
-            foreach my $record (@{$xml->{$records}->{$record}}) {
-                my $data = $record->{$recordData}->{$marcrecord};
+        if ($records) {
+            my $record = first { m/record/ } keys %{$xml->{$records}};
+            my $recordData = first { m/recordData/ } keys %{$xml->{$records}->{$record}};
+            my $marcrecord = first { m/record/ } keys %{$xml->{$records}->{$record}->{$recordData}};
+            
+            if (ref($xml->{$records}->{$record}) eq "HASH") {
+                my $data = $xml->{$records}->{$record}->{$recordData}->{$marcrecord};
                 push @records, $self->convert->formatjson($data);
+            } else {
+                foreach my $record (@{$xml->{$records}->{$record}}) {
+                    my $data = $record->{$recordData}->{$marcrecord};
+                    push @records, $self->convert->formatjson($data);
+                }
             }
         }
 
