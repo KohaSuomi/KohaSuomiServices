cd2d7fd91a4647a16e5a025d26f2a75ebc1a79ee
Johanna Raisa <johanna.raisa@gmail.com>
2018-11-28 08:37:43 +0200

    Adds schema version fix

--- lib/KohaSuomiServices/Database/Biblio/Schema.pm
+++ lib/KohaSuomiServices/Database/Biblio/Schema.pm
@@ -2,7 +2,7 @@ package KohaSuomiServices::Database::Biblio::Schema;
 
 use KohaSuomiServices::Model::Config;
 
-our $VERSION = KohaSuomiServices::Model::Config->new->service("biblio")->load->{database}->{version};
+our $VERSION = defined KohaSuomiServices::Model::Config->new->service("biblio")->load ? KohaSuomiServices::Model::Config->new->service("biblio")->load->{database}->{version} : 1;
 
 use base qw/DBIx::Class::Schema/;
 __PACKAGE__->load_namespaces();
--- lib/KohaSuomiServices/Database/Billing/Schema.pm
+++ lib/KohaSuomiServices/Database/Billing/Schema.pm
@@ -2,7 +2,7 @@ package KohaSuomiServices::Database::Billing::Schema;
 
 use KohaSuomiServices::Model::Config;
 
-our $VERSION = KohaSuomiServices::Model::Config->new->service("billing")->load->{database}->{version};
+our $VERSION = defined KohaSuomiServices::Model::Config->new->service("billing")->load ? KohaSuomiServices::Model::Config->new->service("billing")->load->{database}->{version} : 1;
 
 use base qw/DBIx::Class::Schema/;
 __PACKAGE__->load_namespaces();
