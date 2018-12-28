1b5c76223131c11d1df2e21ed44a6b29e81ece18
Johanna Raisa <johanna.raisa@gmail.com>
2018-12-28 12:08:32 +0200

    Adds fixes to comparison

--- lib/KohaSuomiServices/Model/Compare.pm
+++ lib/KohaSuomiServices/Model/Compare.pm
@@ -10,12 +10,55 @@ use JSON::Patch qw(diff patch);
 sub jsonPatch {
     my ($self, $source, $target) = @_;
 
-    my $diff = diff($source, $target);
-    my $patch;
-    foreach my $d (@{$diff}) {
-        push @{$patch}, $d if $d->{op} eq "add";
+    
+    my $targetpatch = $self->findMandatory($target);
+    my $sourcepatch = $self->findMandatory($source);
+    my $sorted;
+    if ($targetpatch && $sourcepatch) {
+        my $diff = diff($sourcepatch, $targetpatch);
+        warn Data::Dumper::Dumper $diff;
+        patch($sourcepatch, $diff);
+    } elsif ($targetpatch && !$sourcepatch) {
+        foreach my $tfield (@{$targetpatch}) {
+            push @{$source->{fields}}, $tfield;
+        }
+        my $fields = $self->sortFields($source->{fields});
+        $source->{fields} = $fields;
     }
-    patch ($source, $patch);
+}
+
+sub findMandatory {
+    my ($self, $target) = @_;
+
+    my %mandatory = ("500" => 1);
+
+    my $patch;
+    foreach my $field (@{$target->{fields}}) {
+        if ($mandatory{$field->{tag}}) {
+            push @{$patch}, $field;
+        }
+    }
+
+    return $patch;
+}
+
+sub sortFields {
+    my ($self, $fields) = @_;
+
+    my $hash;
+    my $count = 1;
+
+    foreach my $field (@{$fields}) {
+        $hash->{$count} = $field;
+        $count++;
+    }
+
+    my $sorted; 
+     foreach my $key (sort {$hash->{$a}->{'tag'} <=> $hash->{$b}->{'tag'}} keys %$hash) {
+         push @{$sorted}, $hash->{$key};
+    }
+
+    return $sorted;
 }
 
 1;
