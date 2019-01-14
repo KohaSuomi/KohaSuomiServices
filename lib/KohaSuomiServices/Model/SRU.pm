8897985eca7a84b1f32424de86bd4f24d9f91200
Johanna Raisa <johanna.raisa@gmail.com>
2019-01-14 11:15:44 +0200

    Adds exception to sru search

--- lib/KohaSuomiServices/Model/SRU.pm
+++ lib/KohaSuomiServices/Model/SRU.pm
@@ -9,6 +9,7 @@ use List::Util qw<first>;
 use Mojo::JSON qw(decode_json encode_json);
 use Mojo::UserAgent;
 use KohaSuomiServices::Model::Convert;
+use KohaSuomiServices::Model::Exception::NotFound;
 
 has ua => sub {Mojo::UserAgent->new};
 has convert => sub {KohaSuomiServices::Model::Convert->new};
@@ -16,13 +17,13 @@ has convert => sub {KohaSuomiServices::Model::Convert->new};
 sub search {
     my ($self, $params) = @_;
 
+    KohaSuomiServices::Model::Exception::NotFound->throw(error => "SRU query parameter not found") unless $params->{query};
     my $path = $params->{url}."?operation=".$params->{operation}."&query=".$params->{query};
     $path .= defined $params->{version} ? "&version=".$params->{version} : "&version=1.1";
     $path .= defined $params->{maxrecords} ? "&maximumRecords=".$params->{maxrecords} : "&maximumRecords=1";
     my $tx = $self->ua->build_tx(GET => $path);
     $tx = $self->ua->start($tx);
     my $records = $self->getRecords($tx->res->body);
-    warn Data::Dumper::Dumper $records;
     return $records;
 }
 
