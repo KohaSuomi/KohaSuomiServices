0ce958d79352c03a2d54d9a3207d5020ff8345ae
Johanna Raisa <johanna.raisa@gmail.com>
2019-05-27 11:34:58 +0300

    Removes 028 default matchers

--- lib/KohaSuomiServices/Model/Biblio/Matcher.pm
+++ lib/KohaSuomiServices/Model/Biblio/Matcher.pm
@@ -47,7 +47,7 @@ sub find {
 }
 
 sub defaultSearchMatchers {
-    return ("035" => "a", "020" => "a", "024" => "a", "027" => "a", "028" => ["a", "b"]);
+    return ("035" => "a", "020" => "a", "024" => "a", "027" => "a");
 }
 
 sub fetchMatchers {
