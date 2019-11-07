f92257aec3236a0b6ab19c65a675a8929ae28687
Johanna Raisa <johanna.raisa@gmail.com>
2019-11-07 15:11:22 +0200

    Check 028b for query parameters

--- lib/KohaSuomiServices/Model/Biblio/Search.pm
+++ lib/KohaSuomiServices/Model/Biblio/Search.pm
@@ -180,7 +180,7 @@ sub create_query {
     foreach my $param (@{$params}) {
         if($param->{type} eq "query") {
             my @valuematch = $param->{value} =~ /{(.*?)}/g;
-            if (defined $valuematch[0] && $valuematch[0] ne "028a") {
+            if (defined $valuematch[0] && ($valuematch[0] ne "028a" && $valuematch[1] ne "028b")) {
                 my ($key, $value) = %{$matcher} if $matcher;
                 if ($matcher->{$valuematch[0]}) {
                     if ($param->{value} =~ /id=/) {
