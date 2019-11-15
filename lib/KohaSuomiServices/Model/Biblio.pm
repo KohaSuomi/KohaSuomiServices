a6be415f6ab070954e474544cb2eb066c83d767c
Johanna Raisa <johanna.raisa@gmail.com>
2019-11-15 11:03:32 +0200

    Update timestamp in force export

--- lib/KohaSuomiServices/Model/Biblio.pm
+++ lib/KohaSuomiServices/Model/Biblio.pm
@@ -140,7 +140,7 @@ sub pushExport {
 sub forceExport {
     my ($self, $id) = @_;
 
-    $self->exporter->update($id, {status => "pending", force_tag => 1});
+    $self->exporter->update($id, {status => "pending", force_tag => 1, timestamp => \'NOW()'});
     my $schema = $self->schema->client($self->config);
     my @componentparts = $self->exporter->find($schema, {parent_id => $id, errorstatus => 'Parent failed'}, undef);
     if(@componentparts) {
