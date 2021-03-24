0ade1757dd8f5c3443ee1fa3fe1bc539907670ac
Johanna Raisa <johanna.raisa@gmail.com>
2021-03-24 14:20:53 +0200

    Add fix to finding with two matchers

--- lib/KohaSuomiServices/Model/Biblio/Search.pm
+++ lib/KohaSuomiServices/Model/Biblio/Search.pm
@@ -272,9 +272,12 @@ sub create_query {
     foreach my $param (@{$params}) {
         if($param->{type} eq "query") {
             my @valuematch = $param->{value} =~ /{(.*?)}/g;
-            if (defined $valuematch[0] && !$valuematch[1]) {
+            if (defined $valuematch[0]) {
                 my ($key, $value) = %{$matcher} if $matcher;
-                if ($matcher->{$valuematch[0]}) {
+                if ($valuematch[0] eq "003" && $matcher->{$valuematch[0]} && $valuematch[1] eq "001" && $matcher->{$valuematch[1]}) {
+                    $param->{value} =~ s/{$valuematch[0]}/$matcher->{$valuematch[0]}/g;
+                    $param->{value} =~ s/{$valuematch[1]}/$matcher->{$valuematch[1]}/g;
+                } elsif ($matcher->{$valuematch[0]}) {
                     if ($param->{value} =~ /id=/ || $valuematch[0] eq "020a") {
                         $matcher->{$valuematch[0]} =~ s/\D//g;
                     }
@@ -285,10 +288,6 @@ sub create_query {
                     delete $param->{name};
                     delete $param->{value};
                 }
-            } elsif (defined $valuematch[0] && defined $valuematch[1]) {
-                my ($key, $value) = %{$matcher} if $matcher;
-                $param->{value} =~ s/{$valuematch[0]}/$matcher->{$valuematch[0]}/g;
-                $param->{value} =~ s/{$valuematch[1]}/$matcher->{$valuematch[1]}/g;
             }
             if (defined $param->{name} && defined $param->{value}) {
                 $query->{$param->{name}} = $param->{value};
