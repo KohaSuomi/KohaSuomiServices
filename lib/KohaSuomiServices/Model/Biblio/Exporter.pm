b3937ef8207606880c08f19e81b0340f4a099b9b
Johanna Raisa <johanna.raisa@gmail.com>
2021-05-24 14:32:06 +0300

    Add waiting status for finding old exports

--- lib/KohaSuomiServices/Model/Biblio/Exporter.pm
+++ lib/KohaSuomiServices/Model/Biblio/Exporter.pm
@@ -69,7 +69,7 @@ sub setExporterParams {
 sub abortOldExports {
     my ($self, $id) = @_;
     my $client = $self->packages->schema->client($self->packages->config);
-    my @export = $self->find($client, {source_id => $id, status => 'pending'}, undef);
+    my @export = $self->find($client, {source_id => $id, -or => [status => 'waiting', status => 'pending']}, undef);
     return unless @export;
     foreach my $export (@{$self->packages->schema->get_columns(@export)}) {
         $self->packages->log->info("Aborting ".$export->{id}.", newer export record added");
