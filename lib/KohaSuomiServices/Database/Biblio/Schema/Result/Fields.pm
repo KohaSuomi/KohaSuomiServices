c53a85b405ab6bedf39942fb9d8165aa2126dddb
Johanna Raisa <johanna.raisa@gmail.com>
2021-06-03 13:01:00 +0300

    Add default db values

--- lib/KohaSuomiServices/Database/Biblio/Schema/Result/Fields.pm
+++ lib/KohaSuomiServices/Database/Biblio/Schema/Result/Fields.pm
@@ -9,10 +9,10 @@ __PACKAGE__->table('fields');
 __PACKAGE__->add_columns(
   id => { data_type => 'integer',  is_auto_increment => 1 },
   exporter_id => { data_type => 'integer', is_foreign_key => 1 },
-  tag => { data_type => 'varchar', size => 3},
-  ind1 => { data_type => 'varchar', size => 1},
-  ind2 => { data_type => 'varchar', size => 1},
-  value => { data_type => 'varchar', size => 255},
+  tag => { data_type => 'varchar', size => 3, default_value => ''},
+  ind1 => { data_type => 'varchar', size => 1, default_value => ''},
+  ind2 => { data_type => 'varchar', size => 1, default_value => ''},
+  value => { data_type => 'varchar', size => 255, default_value => ''},
   type => { data_type => 'varchar', is_enum => 1, extra => { list => [qw/leader controlfield datafield /]} },
 );
 __PACKAGE__->set_primary_key('id');
