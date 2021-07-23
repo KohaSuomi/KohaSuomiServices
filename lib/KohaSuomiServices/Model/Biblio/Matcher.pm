31c78d6386c964e4dc88f83b5270e5dc4d5654a5
Johanna Raisa <johanna.raisa@gmail.com>
2021-07-23 10:14:57 +0300

    Revert "Revert "Add 035a value check for activation""
    This reverts commit 98ca9f70d15e63d96c2bd408adeb071e2dff1f7b.

--- lib/KohaSuomiServices/Model/Biblio/Matcher.pm
+++ lib/KohaSuomiServices/Model/Biblio/Matcher.pm
@@ -87,7 +87,21 @@ sub targetMatchers {
         $matchers = {};
         if ($weight eq "1") {
             $matchers->{"035a"} = %{$weighted}{$weight};
-            unless ($matchers->{"035a"} =~ /^\(/){
+            if (ref($matchers->{"035a"}) eq "ARRAY") {
+                foreach my $match (@{$matchers->{"035a"}}) {
+                    if ($match =~ /FI-MELINDA/) {
+                        $matchers->{"035a"} = $match;
+                        last;
+                    } elsif ($match =~ /FI-BTJ/) {
+                        $matchers->{"035a"} = $match;
+                        last;
+                    } else {
+                        delete $matchers->{"035a"};
+                        next;
+                    }
+                }
+            }
+            elsif ($matchers->{"035a"} !~ /^\(/){
                 delete $matchers->{"035a"};
                 next;
             }
