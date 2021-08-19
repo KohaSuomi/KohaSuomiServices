f8c0ea2c53dfcb529a50182d76ee2b6d4cb9c989
Johanna Raisa <johanna.raisa@gmail.com>
2021-08-19 09:13:10 +0300

    Select one row for last record

--- lib/KohaSuomiServices/Model/Biblio/ActiveRecords.pm
+++ lib/KohaSuomiServices/Model/Biblio/ActiveRecords.pm
@@ -18,7 +18,7 @@ sub find {
 
 sub findLast {
     my ($self, $client, $params) = @_;
-    return $client->resultset('ActiveRecords')->search($params, {order_by => {-desc => \'CAST(target_id AS int)'}})->next();
+    return $client->resultset('ActiveRecords')->search($params, {order_by => {-desc => \'CAST(target_id AS int)'}, {rows => 1}})->next();
 }
 
 sub insert {
