beabffbed508dde3d433977d1d35ed9141fc8185
Johanna Raisa <johanna.raisa@gmail.com>
2021-10-12 14:09:40 +0300

    Make component parts unique and update them

--- lib/KohaSuomiServices/Database/Biblio/Schema/Result/ComponentParts.pm
+++ lib/KohaSuomiServices/Database/Biblio/Schema/Result/ComponentParts.pm
@@ -7,14 +7,12 @@ use base qw/DBIx::Class::Core/;
 __PACKAGE__->load_components(qw( TimeStamp Core ));
 __PACKAGE__->table('componentparts');
 __PACKAGE__->add_columns(
-  id => { data_type => 'integer',  is_auto_increment => 1 },
-  source_id => { data_type => 'varchar', size => 50, is_nullable => 1 },
+  source_id => { data_type => 'varchar', size => 50 },
   parent_id => { data_type => 'varchar', size => 50, is_nullable => 1 },
   marc => { data_type => 'longtext', default_value => '' },
-  pushed => {data_type => 'integer', is_boolean  => 1, default_value => 0, false_is    => ['0', '-1']},
-  created => { data_type => 'datetime', default_value => \"current_timestamp", is_nullable => 0},
+  updated => { data_type => 'datetime', is_nullable => 0}
 );
-__PACKAGE__->set_primary_key('id');
+__PACKAGE__->set_primary_key('source_id');
 ## ## ##   DONE WITH DBIx::Schema   ## ## ##
 ############################################
 
