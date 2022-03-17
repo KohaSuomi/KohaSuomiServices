3b3de3563e2585c6a4f394400b7b178403b62c6c
Johanna Raisa <johanna.raisa@gmail.com>
2022-03-17 13:50:07 +0200

    Change user authentication methods

--- lib/KohaSuomiServices/Model/Auth.pm
+++ lib/KohaSuomiServices/Model/Auth.pm
@@ -9,11 +9,15 @@ use Try::Tiny;
 use KohaSuomiServices::Model::Config;
 use Mojo::JSON qw(decode_json encode_json from_json);
 use KohaSuomiServices::Model::Exception::Unauthorized;
+use C4::Auth qw( haspermission );
+use Mojo::URL;
+use Cache::FastMmap;
 
 has schema => sub {KohaSuomiServices::Database::Client->new};
 has ua => sub {Mojo::UserAgent->new};
 has config => sub {KohaSuomiServices::Model::Config->new->load};
 has session => sub {KohaSuomiServices::Model::Auth::Session->new};
+has cache => sub {Cache::FastMmap->new(share_file => '/tmp/ks_sessions', cache_size => '1h')};
 
 sub valid {
     my ($self, $token) = @_;
@@ -24,22 +28,22 @@ sub valid {
 sub login {
     my ($self, $username, $password) = @_;
     my $path = $self->config->{auth}->{internallogin};
+    $path = Mojo::URL->new($path)->userinfo($username.':'.$password);
     my $error;
-    my $params = {userid => $username, password => $password};
-    my $tx = $self->ua->build_tx(POST => $path => {Accept => 'application/x-www-form-urlencoded'} => form => $params);
+    my $params = {userid => $username};
+    my $tx = $self->ua->build_tx(GET => $path => {Accept => 'application/x-www-form-urlencoded'} => form => $params);
     $tx = $self->ua->start($tx);
     $error = {code => defined $tx->res->error->{code} && $tx->res->error->{code} ? $tx->res->error->{code} : 500, message => from_json($tx->res->body)} if defined $tx->res->error && $tx->res->error;
-    return ($self->checkPermissions(decode_json($tx->res->body)), $error);
+    my $user = decode_json($tx->res->body);
+    $user = shift @$user;
+    my $hash = $self->checkPermissions($user);
+    $self->cache->set($hash, $user->{userid});
+    return ($hash, $error);
 }
 
 sub get {
     my ($self, $sessionid) = @_;
-
-    my $path = $self->config->{auth}->{internallogin};
-    my $params = {sessionid => $sessionid};
-    my $tx = $self->ua->build_tx(GET => $path => {Accept => 'application/json', "Cookie: " => "CGISESSID=".$sessionid} => json => $params);
-    $tx = $self->ua->start($tx);
-    return $self->checkPermissions(decode_json($tx->res->body));
+    return $self->cache->get($sessionid);
 }
 
 sub delete {
@@ -52,14 +56,16 @@ sub delete {
 }
 
 sub checkPermissions {
-    my ($self, $params) = @_;
-    
-    foreach my $permission (@{$self->config->{auth}->{permissions}}) {
-        if ($permission ~~ @{$params->{permissions}}) {
-            return $params->{sessionid};
-        }
-    }
+    my ($self, $user) = @_;
+    my $haspermission = C4::Auth::haspermission($user->{userid});
+    return $self->userHash($user) if $haspermission->{superlibrarian};
     return 0;
+}
+
+sub userHash {
+    my ($self, $user) = @_;
+    my $hash = Digest::SHA::hmac_sha256_hex($user->{userid}.$user->{updated_on});
+    return $hash;
 }
 
 1;
